96532cfa59c2f1810bfd05173bda7c40da3d7f38
Raghavendra Saralaya <raghavendra.saralaya@gmail.com>
2021-09-17 17:15:13 +0530

    separated out authenticated vs. not for root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-
+  
+  
+  
   resources :documents 
-
+  
+  authenticated :user do
+    root 'documents#index', as: 'authenticated_root'
+  end
+  
+  root 'welcome#index'
 end
